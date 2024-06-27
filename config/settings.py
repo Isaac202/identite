a2eb49f81d284043ba618629d668c82234e02c51
Isaac202 <isaacbyguaracity@gmail.com>
2024-06-27 13:48:00 -0300

    Ajustes finais

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z#4gr!e)jpwar1$wia^c%5p_j2%&^sgi9m7m8g3dc4$)$)9g9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
