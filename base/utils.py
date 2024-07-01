ca3918239c13b991307ab9db7d2a1a84631c554e
Isaac202 <isaacbyguaracity@gmail.com>
2024-07-01 12:49:55 -0300

    Ajustes

--- base/utils.py
+++ base/utils.py
@@ -16,85 +16,10 @@ def decrypt_voucher(encrypted_voucher):
     decrypted_voucher = fernet.decrypt(encrypted_voucher.encode()).decode()
     return decrypted_voucher
 
-'''
-
-
-{
-	"apiKey": "123ChaveDisponibilizadaPeloSistema456",
-    "HashVendedor": "1d3ntific4d0r-V3ndend0r",
-    "HashTabela": "1d3ntific4d0r-T4bela",
-    "FormaPagamento": 8,
-    "CodigoVoucher": "",
-    "Cliente":{
-    	"CNPJCPF": "19999999992",
-    	"NomeRazaoSocial": "Paulo Henrique Fraga Mathias Netto",
-    	"NomeFantasia": "",
-    	"Email": "paulo***************.com.br",
-    	"CEP": "90450120",
-    	"Logradouro": "Rua Carlos Trein Filho",
-    	"Numero": "627",
-    	"Complemento": "",
-    	"Bairro": "Mont Serrat",
-    	"Cidade": "Porto Alegre",
-    	"UF": "RS",
-    	"CodigoIBGE": "4314902",
-    	"DDD": 51,
-    	"Telefone": 989999912
-    },
-    "Produtos": [
-        {
-            "HashProduto": "1d3ntific4d0r-Pr0duto"
-        },
-        {
-            "HashProduto": "1d3ntific4d0r-0utro-Pr0duto"
-        }
-    ]
-}
-'''
-
-import json
+url = settings.URL_API
 
 def salvar_venda(cliente):
-    endpoint = 'https://apiconsulti.gestaoar.com.br/consultibrasil/'
-    apiKey = "e5aaffae75484e138cd1685e2f486b54452edf0e867a4fda8e7bdd8f16b92502"
-    HashVendedor=  "01d6e9ff-3b53-4ba4-b9a7-f0ea9c9d4157"
-    HashTabela = "86d7f05b-a75d-4e1f-b4a2-3558424e678a"
-    FormaPagamento = 8
-    CodigoVoucher = cliente.voucher.code
-
-    headers = {'Content-Type': 'application/json'}
-
-    payload = {
-        "apiKey": apiKey,
-        "HashVendedor": HashVendedor,
-        "HashTabela": HashTabela,
-        "FormaPagamento": FormaPagamento,
-        "CodigoVoucher": CodigoVoucher,
-        "Cliente":{
-            "CNPJCPF": cliente.cpf if cliente.cnpj == None else cliente.cnpj,
-            "NomeRazaoSocial": cliente.razao_social,
-            "NomeFantasia": cliente.nome_fantasia,
-            "Email": cliente.email,
-            "CEP": cliente.cep,
-            "Logradouro": cliente.logradouro,
-            "Numero": cliente.numero,
-            "Complemento":cliente.complemento,
-            "Bairro": cliente.bairro,
-            "Cidade": cliente.cidade,
-            "UF": cliente.uf,
-            "CodigoIBGE": cliente.cod_ibge,
-            "DDD": cliente.telefone[0:2],
-            "Telefone": cliente.telefone
-        },
-        "Produtos": [
-            {
-                "HashProduto": "e9eaa186-f11e-4a08-9f22-387c6c60e035"
-            }
-        ]
-    }
-
-def salvar_venda(cliente):
-    endpoint = 'https://apiconsulti.gestaoar.com.br/consultibrasil/api/GarAPIs/'
+    endpoint = f'{url}/api/GarAPIs/'
     apiKey = "e5aaffae75484e138cd1685e2f486b54452edf0e867a4fda8e7bdd8f16b92502"
     HashVendedor=  "01d6e9ff-3b53-4ba4-b9a7-f0ea9c9d4157"
     HashTabela = "86d7f05b-a75d-4e1f-b4a2-3558424e678a"
@@ -154,7 +79,7 @@ def salvar_venda(cliente):
 
 
 def gerar_protocolo(pedido, cnpj, cpf, data_nascimento, is_possui_cnh):
-    url = 'https://apiconsulti.gestaoar.com.br/consultibrasil/api/GarAPIs/'  # Substitua pela URL da API que gera os protocolos
+    url = f'{url}/api/GarAPIs/'  # Substitua pela URL da API que gera os protocolos
     headers = {"Content-Type": "application/json"}
     payload = {
         "apiKey": "e5aaffae75484e138cd1685e2f486b54452edf0e867a4fda8e7bdd8f16b92502",
@@ -184,7 +109,7 @@ def gerar_protocolo(pedido, cnpj, cpf, data_nascimento, is_possui_cnh):
 
 
 def obter_disponibilidade_agenda():
-    url = 'https://apiconsulti.gestaoar.com.br/consultibrasil/api/GarAPIs/ObterDisponibilidadeAgenda'
+    url = f'{url}/api/GarAPIs/ObterDisponibilidadeAgenda'
     headers = {"Content-Type": "application/json"}
     payload = {
        "apiKey": "e5aaffae75484e138cd1685e2f486b54452edf0e867a4fda8e7bdd8f16b92502",
@@ -206,7 +131,7 @@ def obter_disponibilidade_agenda():
 
 
 def agendar_pedido( hash_venda, data, hora_inicial, hora_final):
-    url = 'https://apiconsulti.gestaoar.com.br/consultibrasil/api/GarAPIs/AgendarPedido'
+    url = f'{url}/api/GarAPIs/AgendarPedido'
     headers = {"Content-Type": "application/json"}
     payload = { 
         "apiKey": "e5aaffae75484e138cd1685e2f486b54452edf0e867a4fda8e7bdd8f16b92502",
@@ -229,7 +154,7 @@ def agendar_pedido( hash_venda, data, hora_inicial, hora_final):
 
 
 def consultar_status_pedido(pedido):
-    url = 'https://apiconsulti.gestaoar.com.br/consultibrasil/api/GarAPIs/ConsultaPedidoProtocolo'
+    url = f'{url}/api/GarAPIs/ConsultaPedidoProtocolo'
     headers = {"Content-Type": "application/json"}
     payload = { 
                 "apiKey": "e5aaffae75484e138cd1685e2f486b54452edf0e867a4fda8e7bdd8f16b92502",
--- config/settings.py
+++ config/settings.py
@@ -179,7 +179,7 @@ LOGGING = {
         },
     }
 }
-
+URL_API = config('URL_API')
 
 APPEND_SLASH = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
