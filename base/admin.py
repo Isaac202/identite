1856b624f1986ea7e239eed5af12e8d027a6ec02
Isaac202 <isaacbyguaracity@gmail.com>
2024-07-11 20:40:41 -0300

    Ajustes no admin

--- base/admin.py
+++ base/admin.py
@@ -4,6 +4,6 @@ from .models import DadosCliente, Pedidos, Voucher
 admin.site.register(Voucher)
 admin.site.register(Pedidos)
 class DadosClienteAdmin(admin.ModelAdmin):
-    search_fields = ['nome', 'cpf']
+    search_fields = ['nome_completo', 'cpf']
 
 admin.site.register(DadosCliente, DadosClienteAdmin)
