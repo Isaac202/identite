a6640a38500cd065168780ba3e4c628ddbda87b1
Isaac202 <isaacbyguaracity@gmail.com>
2024-09-30 09:46:31 -0300

    Agenda

--- base/utils.py
+++ base/utils.py
@@ -114,7 +114,7 @@ def obter_disponibilidade_agenda():
     payload = {
        "apiKey": API_KEY,
         "DataInicial": "2024-06-27",
-        "DataFinal": "2024-09-29",
+        "DataFinal": "2024-10-29",
         "hashLocal": "01d6e9ff-3b53-4ba4-b9a7-f0ea9c9d4157"
     }
 
